f7f6ad300c3babff57f9c4143f15b6f76f278619
elton-cs <govada.elton@gmail.com>
2024-01-27 00:56:59 -0600

    Test simple stream transfer

--- contracts/src/Flow.ts
+++ contracts/src/Flow.ts
@@ -1,10 +1,31 @@
-import { Field, SmartContract, State, UInt64, method, state } from "o1js";
+import { Field, SmartContract, State, UInt64, assert, method, state } from "o1js";
 
 export class Flow extends SmartContract {
     @state(UInt64) tokenValue = State<UInt64>();
+    @state(UInt64) fromTokens = State<UInt64>();
+    @state(UInt64) toTokens = State<UInt64>();
+
+    init() {
+        super.init();
+        this.fromTokens.set(UInt64.from(10))
+        this.toTokens.set(UInt64.from(0))
+    }
 
     @method update(value: UInt64) {
         this.tokenValue.getAndRequireEquals();
         this.tokenValue.set(value)
+    }
+
+    @method streamTransfer() {
+        let ft = this.fromTokens.getAndRequireEquals()
+        let tt = this.toTokens.getAndRequireEquals()
+
+        ft.assertGreaterThan(UInt64.from(0))
+        let newft = ft.sub(1)
+        let newtt = tt.add(1)
+
+        this.fromTokens.set(newft)
+        this.toTokens.set(newtt)
+        
     }
 }
--- contracts/src/interact.ts
+++ contracts/src/interact.ts
@@ -1,22 +1,21 @@
-import { Account, AccountUpdate, Mina, PrivateKey } from "o1js";
-
 // SMART CONTRACT
+// import { Field, SmartContract, State, UInt64, method, state } from "o1js";
 
-import { Field, SmartContract, State, UInt64, method, state } from "o1js";
+// export class Flow extends SmartContract {
+//     @state(UInt64) tokenValue = State<UInt64>();
 
-export class Flow extends SmartContract {
-    @state(UInt64) tokenValue = State<UInt64>();
+//     @method update(value: UInt64) {
+//         this.tokenValue.getAndRequireEquals();
+//         this.tokenValue.set(value)
+//     }
+// }
 
-    @method update(value: UInt64) {
-        this.tokenValue.getAndRequireEquals();
-        this.tokenValue.set(value)
-    }
-}
+
+import { Account, AccountUpdate, Mina, PrivateKey, UInt64 } from "o1js";
+import { Flow } from "./Flow";
 
 // INTERACTION:
-
 const useProof = false;
-
 const Local = Mina.LocalBlockchain({proofsEnabled: useProof})
 
 Mina.setActiveInstance(Local);
@@ -26,33 +25,34 @@ const hunter = Local.testAccounts[1];
 const deployer = Local.testAccounts[2];
 
 const ZK_APP_PRIVATE_KEY = PrivateKey.fromBase58('EKEYXmPQTTTT2vFKeBm769prz8yUVc5Gb6QnoM2d98aMv3fmrYSG');
+
 const zkAppAddress = ZK_APP_PRIVATE_KEY.toPublicKey();
 console.log(zkAppAddress.toBase58())
 
+
 const zkApp = new Flow(zkAppAddress);
+await Flow.compile();
+
 const deployZkAppTxn = await Mina.transaction( deployer.publicKey, () => {
     AccountUpdate.fundNewAccount(deployer.publicKey);
     zkApp.deploy();
 });
 await deployZkAppTxn.sign([deployer.privateKey, ZK_APP_PRIVATE_KEY]).send();
 
-let tokenValue = zkApp.tokenValue.get();
-console.log(tokenValue.toString())
 
-
-const setNewValueTxn = await Mina.transaction( builder.publicKey, () => {
-    zkApp.update(UInt64.from(1000))
-});
-await setNewValueTxn.prove();
-await setNewValueTxn.sign([builder.privateKey]).send();
-
-
-tokenValue = zkApp.tokenValue.get();
-console.log(tokenValue.toString())
-
-let currTimeStamp = zkApp.network.timestamp.get()
-let value = 1706321445049
+export let currTimeStamp = zkApp.network.timestamp.get()
 console.log(currTimeStamp.toString())
-let difff = currTimeStamp.sub(UInt64.from(1706321445049)).div(UInt64.from(10)).toString();
 
-console.log(`difference: ${difff} `);
+let from = zkApp.fromTokens.get().toString();
+let to = zkApp.toTokens.get().toString();
+console.log(`${from} -> ${to}`)
+
+const streamTransferTXN = await Mina.transaction( builder.publicKey, () => {
+    zkApp.streamTransfer();
+});
+await streamTransferTXN.prove();
+await streamTransferTXN.sign([builder.privateKey]).send();
+
+from = zkApp.fromTokens.get().toString();
+to = zkApp.toTokens.get().toString();
+console.log(`${from} -> ${to}`)
